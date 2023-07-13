a5adbbee5a1ecfa912e3e88b9e985b1e6d4a1f3a
umar545 <muhammadumarfarooq545@gmail.com>
2023-07-13 08:54:53 +0500

    repositry created

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>home</li>
+        <li>about us</li>
+        <li>contact</li>
+        <li>blogs</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1 className="home-heading">intro to react router dom v6</h1>
+      <h3>What is react router</h3>
+      <p>React Router is library used for navigation in React</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque in
+        blanditiis vero voluptates distinctio sunt sed dolorem autem vel
+        eligendi dicta laudantium similique obcaecati et quaerat, iure illum
+        atque eum!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
